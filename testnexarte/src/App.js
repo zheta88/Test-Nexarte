c307e31d0e3a30ef94da72e6ecf8b93c9b231693
Zully Tamayo M <52215922+zheta88@users.noreply.github.com>
2022-04-20 21:30:34 -0500

    calculadora punto numeral a

--- testnexarte/src/App.js
+++ testnexarte/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from "react";
+import { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {operacion:0,operando1:'', operando2: ''};
+    this.resultado=null;
+  }
+
+
+  onInputChange = (operando,val) => {
+    console.log(this.state);
+    if(operando === 'operando1')
+      this.setState({...this.state, operando1: parseInt(val)});
+    else if(operando === 'operando2')
+      this.setState({...this.state, operando2: parseInt(val)});
+  }
+
+  calcular =()=>{
+    const message = document.getElementById("res");
+    message.innerHTML = "";
+      if(this.state.operacion==0){
+        alert("Seleccione operacion a realizar");
+        this.setState({...this.state});
+      }
+      else if(this.state.operacion==='1'){
+        this.resultado = (this.state.operando1+this.state.operando2);
+        this.setState({...this.state});
+      }
+      else if(this.state.operacion==='2'){
+        this.resultado = (this.state.operando1-this.state.operando2);
+        this.setState({...this.state});
+      }
+      else if(this.state.operacion==='3'){
+        this.resultado = (this.state.operando1*this.state.operando2);
+        this.setState({...this.state});
+      }
+      else if(this.state.operacion==='4'){
+        this.resultado = (this.state.operando1/this.state.operando2);
+        this.setState({...this.state});
+      }
+    try { 
+      if(this.state.operando1=== "" || this.state.operando2=== "")  throw "Ingrese valores";
+    }
+    catch(err) {
+      message.innerHTML =  err;
+    }
+   
+  }
+  render(){
+    return (
+
+      <div className="App">
+        <div className="card">
+          <div className="form-group">
+            <select className="custom-select" onChange={event => this.setState({...this.state,operacion:event.target.value})}>
+            <option value="0">Elija Operacion</option>
+              <option value="1">Sumar</option>
+              <option value="2">Restar</option>
+              <option value="3">Multiplicar</option>
+              <option value="4">Dividir</option>
+            </select>
+          </div>
+          <input className="input" type="number" value={this.state.operando1} onChange={(event) => this.onInputChange('operando1',event.target.value)}/>
+          <input className="input" type="number" value={this.state.operando2} onChange={(event) => this.onInputChange('operando2',event.target.value)}/>
+          <span id="res">{this.resultado}</span>
+          <button className="button" onClick={this.calcular}>Calcular</button>
+        </div>
+        
+      </div>
+      
+
+    );
+       
+  }
+  
 }
 
 export default App;
